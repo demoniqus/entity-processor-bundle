4d927f39052d4dd89476e3a04be75c32a33258cd
antipda <antipda@ite-ng.ru>
2024-02-05 09:29:47 +0300

    Add Bundle file

---
+++ src/EntityProcessorBundle.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Demoniqus\EntityProcessor;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EntityProcessorBundle extends Bundle
+{
+//region SECTION: Fields
+
+//endregion Fields
+
+//region SECTION: Constructor
+
+//endregion Constructor 
+
+//region SECTION: Protected 
+
+//endregion Protected
+
+//region SECTION: Public
+
+//endregion Public
+
+//region SECTION: Getters/Setters 
+
+//endregion Getters/Setters
+}
