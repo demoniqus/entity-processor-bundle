c86f00e44a4a066b3b5d3f20a21e2afaba99b195
antipda <antipda@ite-ng.ru>
2024-02-07 10:36:05 +0300

    fix

---
+++ src/Traits/ValidatorTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Demoniqus\EntityProcessor\Traits;
+
+
+trait ValidatorTrait
+{
+//region SECTION: Fields
+
+//endregion Fields
+
+//region SECTION: Constructor
+
+//endregion Constructor
+
+//region SECTION: Protected
+	/**
+	 * @param string[] $errors
+	 */
+	protected function addErrors(array $errors): void
+	{
+		foreach ($errors as $error) {
+			$this->addError($error);
+		}
+	}
+//endregion Protected
+
+//region SECTION: Public
+
+//endregion Public
+
+//region SECTION: Getters/Setters
+
+//endregion Getters/Setters
+}
